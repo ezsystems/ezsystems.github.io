09bc1f69be2e7aa5c0f230a615a6f6bf63d149cc
eZ Robot, I do what I'm told to <ezrobot@ez.no>
2016-11-04 11:00:56 +0000

    Updated PlatformUI API doc
    After:
    6ab71e0 Merge pull request #714 from ezsystems/ezp-26430-translator_shortcut

--- platformui-javascript-api/api.js
+++ platformui-javascript-api/api.js
@@ -215,6 +215,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "eZ.TextBlockEditView",
         "eZ.TextLineEditView",
         "eZ.TranslateActionView",
+        "eZ.Translator",
         "eZ.TrashBarView",
         "eZ.TrashItem",
         "eZ.TrashView",
@@ -256,6 +257,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "ez-alloyeditor-button-embedcenter",
         "ez-alloyeditor-button-embedleft",
         "ez-alloyeditor-button-imagehref",
+        "ez-alloyeditor-button-imagevariation",
         "ez-alloyeditor-button-mixin-blocktextalign",
         "ez-alloyeditor-button-mixin-embedalign",
         "ez-alloyeditor-button-mixin-embeddiscovercontent",
@@ -503,6 +505,11 @@ YUI.add("yuidoc-meta", function(Y) {
             "description": "Provides the ezimagehref button"
         },
         {
+            "displayName": "ez-alloyeditor-button-imagevariation",
+            "name": "ez-alloyeditor-button-imagevariation",
+            "description": "Provides the image variation drop down"
+        },
+        {
             "displayName": "ez-alloyeditor-button-mixin-blocktextalign",
             "name": "ez-alloyeditor-button-mixin-blocktextalign",
             "description": "Provides the BlockTextAlign mixin"
@@ -530,7 +537,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "ez-alloyeditor-button-remove",
             "name": "ez-alloyeditor-button-remove",
-            "description": "Provides the image variation drop down"
+            "description": "Provides the embedhref href button"
         },
         {
             "displayName": "ez-alloyeditor-toolbar-config-block-base",
